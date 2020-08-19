75a41934df68a9f9792963269199fb87e972f74a
Omri Avisar <omriavisar@mail.tau.ac.il>
2020-08-19 19:41:37 +0300

    Remove learning's tests for their old brain

--- tests/learning/test_lazy_brain/lazy_brain_test_base.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from tests.learning.brain_test_utils import BrainTestUtils
-
-
-class LazyBrainTestBase(unittest.TestCase):
-
-    def setUp(self) -> None:
-        self.utils = BrainTestUtils(lazy=True)
--- tests/learning/test_non_lazy_brain/non_lazy_brain_test_base.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from tests.brain_test_utils import BrainTestUtils
-
-
-class NonLazyBrainTestBase(unittest.TestCase):
-
-    def setUp(self) -> None:
-        self.utils = BrainTestUtils(lazy=False)
